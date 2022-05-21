98a12f4b682d2812b0644f347c6d2d050c731789
혀느현스 <hyuns@hyuns.dev>
2022-05-21 21:46:50 +0900

    아맞다커밋

---
+++ src/components/layout/app/sidebar.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+
+const Divver = styled.div`
+    width: 58px;
+    height: 100%;
+    background-color: var(--gray1);
+`
+
+interface SidebarProps {
+
+}
+
+export function Sidebar(props: SidebarProps) {
+
+    return (
+        <Divver>
+
+        </Divver>
+    );
+}
