68906bc976bf14fa0e633008d0dcef6e3bb6d9c8
혀느현스 <hyuns@hyuns.dev>
2022-05-29 05:22:30 +0900

    table

--- src/components/table/tableStyle.tsx
+++ src/components/table/tableStyle.tsx
@@ -1,13 +1,56 @@
+import styled from "styled-components"
 
+const Divver = styled.div`
+    display: block;
+    overflow: auto;
+`
+
+const TableStyle = styled.table`
+    width: 100%;
+    border-collapse: collapse;
+    box-sizing: border-box;
+    font-size: 14px;
+
+    th {
+        text-align: left;
+        
+    }
+
+    td {
+        border-top: 1px solid var(--gray4);
+        border-bottom: 1px solid var(--gray4);
+        box-sizing: border-box;
+
+        img {
+            width: 28px;
+            display: block;
+        }
+    } 
+
+    th, td {
+        display: flex;
+        align-items: center;
+        box-sizing: border-box;
+        padding: 4px 8px;
+        
+        div {
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+        }
+    }
+`
 
 interface TableProps {
-
+    children: React.ReactElement[]
 }
 
-export function TableStyle(props: TableProps) {
+export function Table(props: TableProps) {
     return (
-        <>
-            
-        </>
+        <Divver>
+            <TableStyle>
+                {props.children}
+            </TableStyle>
+        </Divver>
     )
 }
--- src/pages/app/channel/var/varChannel.tsx
+++ src/pages/app/channel/var/varChannel.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useMemo, useState } from "react";
-import { useTable } from 'react-table';
+import { useTable, useFlexLayout, useBlockLayout } from 'react-table';
 import styled from "styled-components";
 import { SearchBox } from "../../../../components/search/searchBox";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 import { H1, TabDivver } from "../../../../components/Tab";
+import { Table } from "../../../../components/table/tableStyle";
+
 
 const Divver = styled.div`
     width: 100%;
@@ -11,8 +15,17 @@ const Divver = styled.div`
 
 const columnsData = [
     {
+        accessor: 'img',
+        Header: '',
+        Cell: (row: any) => <img src={row.row.original.img} alt="" className="TableImage"></img>,
+        width: 64,
+        maxWidth: 64
+    },
+    {
         accessor: 'key',
-        Header: '키'
+        Header: '키',
+        width: 100,
+        maxWidth: 200,
     },
     {
         accessor: 'value',
@@ -20,35 +33,83 @@ const columnsData = [
     },
     {
         accessor: 'isPublic',
-        Header: '공개'
-    },
-    {
-        accessor: 'img',
-        Header: ''
+        Header: '공개',
+        width: 80,
+        maxWidth: 80,
+        Cell: (row: any) => <>{row.row.original.isPublic ? '공개' : '비공개'}</>,
     },
 ]
 
 export function VarChannel() {
-    const [ searchText, setSearchText ] = useState('')
+    const [searchText, setSearchText] = useState('')
     const columns = useMemo(() => columnsData, [])
-    const { isLoading: serverStatusLoading,  data:serverStatus } = useQuery(['serversStatus'], async () => {
-        const res = await axios.get(`${process.env.REACT_APP_API_URL}/server`, {
+
+    const { isLoading: varLoading, data: varData } = useQuery(['var'], async () => {
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/var`, {
             headers: {
                 Authorization: localStorage.getItem('token') || ''
             }
         })
-
         return res.data
     })
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns: columns, data: varData });
+    const {
+        getTableProps, headerGroups, rows, prepareRow
+    } = useTable(
+        {
+            columns,
+            data: varLoading ? [] : varData,
+            // defaultColumn
+        },
+        useFlexLayout,
+    );
 
     return (
         <Divver>
             <TabDivver>
                 <H1>변수</H1>
                 <SearchBox value={searchText} onChange={setSearchText} />
-                
+                <Table {...getTableProps()}>
+                    <thead>
+                        {
+                            headerGroups.map(headerGroup => (
+                                <tr {...headerGroup.getHeaderGroupProps()}>
+                                    {
+                                        headerGroup.headers.map(column => (
+                                            <th {...column.getHeaderProps({
+                                                style: { minWidth: column.minWidth, width: column.width, maxWidth: column.maxWidth },
+                                            })}>
+                                                <div>
+                                                    {column.render('Header')}
+                                                </div>
+                                            </th>
+                                        ))}
+                                </tr>
+                            ))}
+                    </thead>
+                    <tbody>
+                        {
+                            rows.map(row => {
+                                prepareRow(row)
+                                return (
+                                    <tr {...row.getRowProps()}>
+                                        {
+                                            row.cells.map(cell => {
+                                                return (
+                                                    <td {...cell.getCellProps({
+                                                        style: { minWidth: cell.column.minWidth, width: cell.column.width, maxWidth: cell.column.maxWidth },
+                                                    })}>
+                                                        <div>
+                                                            { cell.render('Cell') }
+                                                        </div>
+                                                    </td>
+                                                )
+                                            })}
+                                    </tr>
+                                )
+                            })}
+                    </tbody>
+                </Table>
             </TabDivver>
         </Divver>
     )
